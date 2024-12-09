797ea8ab8e3c1f4a84767203c84fb5d00152c4a9
Robert Wiley <ylee@bodhilinux.com>
2024-12-09 09:31:44 -0500

    Change to meson build

--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -1,3 +1,4 @@
+#include "config.h"
 #include <e.h>
 #include "e_mod_main.h"
 
@@ -297,11 +298,9 @@ EAPI E_Module_Api e_modapi =
 EAPI void *
 e_modapi_init(E_Module *m) 
 {
-   char buf[PATH_MAX];
-
-   snprintf(buf, sizeof(buf), "%s/locale", e_module_dir_get(m));
-   bindtextdomain(PACKAGE, buf);
-   bind_textdomain_codeset(PACKAGE, "UTF-8");
+   /* Set up module locales*/
+   bindtextdomain(LOCALEDOMAIN, LOCALEDIR);
+   bind_textdomain_codeset(LOCALEDOMAIN, "UTF-8");
 
    conf_item_edd = E_CONFIG_DD_NEW("Separator_Config_Item", Config_Item);
    conf_edd = E_CONFIG_DD_NEW("Separator_Config", Config);
